b66768233752627c555dd0c50620911dd4ed6a4b
Chris Bolton <chris8bolton@icloud.com>
2023-10-11 13:44:24 -0400

    add player game-by-game

--- src/app.py
+++ src/app.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify, request
 import requests
 import json
 from markupsafe import escape_silent
+import datetime
 
 app = Flask(__name__)
+
+CURRENT_SEASON = '20232024'
+SEASON_START = '2023-10-10'
 
 @app.route('/teams/<string:teamName>/stats')
 def getTeamStats(teamName=None):
@@ -48,9 +52,60 @@ def getPlayerStats(playerName=None):
 
     return jsonify(data['stats'])
 
-@app.route('/players/<string:playerName>/accolades')
-def getPlayerAccolades(playerName=None):
-    return
+@app.route('/players/<string:playerName>/gameByGame')
+def getPlayerGameByGame(playerName=None):
+    team_ids = getPlayerTeams(playerName)
+    player_id = getPlayerIDByName(playerName)
+
+    last = escape_silent(request.args.get('last'))
+
+    playerSeasonStats = []
+
+    today = datetime.date.today().strftime("%Y-%m-%d")
+
+    for team_id in team_ids:
+        data = requests.get(f'https://statsapi.web.nhl.com/api/v1/schedule?teamId={team_id}&startDate={SEASON_START}&endDate={today}')
+        if data.status_code != 200:
+            continue
+        else:
+            data = data.json()
+
+        games = data['dates']
+
+        for game in games:
+            game_data = game['games'][0]
+
+            game_id = game_data['gamePk']
+
+            # get boxscore game
+
+            data = requests.get(f'https://statsapi.web.nhl.com/api/v1/game/{game_id}/boxscore')
+            if data.status_code != 200:
+                continue
+            else:
+                data = data.json()
+
+            game_players = {}
+            if data['teams']['away']['team']['id'] == team_id:
+                game_players = data['teams']['away']['players']
+            else:
+                game_players = data['teams']['home']['players']
+
+            try:
+                player_game_data = game_players['ID' + str(player_id)]
+                playerSeasonStats.append(player_game_data['stats'])
+            except:
+                continue
+
+    n = len(playerSeasonStats)
+
+    try:
+        last = int(last)
+    except:
+        last = n
+        
+    playerSeasonStats = playerSeasonStats[(n - last)::]
+    return jsonify(playerSeasonStats)
 
 def getTeamIDByName(team_name):
     data = requests.get('https://statsapi.web.nhl.com/api/v1/teams')
@@ -81,6 +136,32 @@ def getPlayerIDByName(player_name):
         retval = None
 
     return retval
+
+def getPlayerTeams(playerName, season=None):
+
+    if season == None:
+        season = CURRENT_SEASON
+
+    team_ids = []
+
+    playerID = getPlayerIDByName(playerName)
+
+    data = requests.get(f'https://statsapi.web.nhl.com/api/v1/people/{playerID}/stats?stats=yearByYear')
+    if data.status_code != 200:
+        return '[]'
+    else:
+        data = data.json()
+
+    yearByYear = data['stats'][0]['splits']
+
+    for year in yearByYear:
+        thisSeason = year['season']
+
+        if thisSeason == season:
+            team_ids.append(year['team']['id'])
+
+    return team_ids
+    
 
 if __name__ == '__main__':
     app.run()
